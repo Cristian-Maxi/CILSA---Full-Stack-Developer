68e1b214c01934fcb7e9c20bcdc782bf026c8c02
Cristian-Maxi <cristiangomez0686@outlook.com>
2024-08-23 13:36:15 -0300

    Modificacion Tarea 4

--- Ejercicio 4/Ejercicio 4.js
+++ Ejercicio 4/Ejercicio 4.js
@@ -5,3 +5,32 @@ function ModoOscuro() {
     document.querySelectorAll("input").forEach(input => input.classList.toggle("modo-oscuro"));
     document.querySelector("#boton-oscuro").classList.toggle("modo-oscuro")
 }
+
+function ComprobarCampos(event) {
+    let nombre = document.getElementById("nombre").value;
+    let apellido = document.getElementById("apellido").value;
+    let pais = document.getElementById("pais").value;
+
+    const regex = /\d/; // Expresión regular para buscar números
+
+    if (regex.test(nombre)) {
+        alert("Error: El campo Nombre no debe contener números.");
+        event.preventDefault(); // Evita el envío del formulario
+        return false;
+    }
+
+    if (regex.test(apellido)) {
+        alert("Error: El campo Apellido no debe contener números.");
+        event.preventDefault(); // Evita el envío del formulario
+        return false;
+    }
+
+    if (regex.test(pais)) {
+        alert("Error: El campo Pais no debe contener números.");
+        event.preventDefault(); // Evita el envío del formulario
+        return false;
+    }
+    
+
+    return true; // Permite el envío del formulario si no hay números en los campos
+}
